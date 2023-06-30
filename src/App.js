20372c6818d90aa7f55ddc17a428d36faff54130
jeswinsilvaan <137885700+jeswinsilvaan@users.noreply.github.com>
2023-06-30 09:45:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NameList from './NameList';
+
+export default function App() {
+  return (
+    <div>
+      <h1>ADD YOUR NAME</h1>
+      <NameList />
+    </div>
+  );
+}
+
